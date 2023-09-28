175f90b0dce182e234fdad7bedb24acf5117a1dc
Sameer Yadav <sameer@Sameers-MacBook-Air.local>
2023-09-28 12:05:09 +0530

    using exrate for exchange rate

--- src/utils/apiRoutes.js
+++ src/utils/apiRoutes.js
@@ -1,67 +1,67 @@
 export function apiRoutes() {
   // localhost
-  const loginAPI = "http://localhost:9002/api/login";
-  const registerAPI = "http://localhost:9002/api/register";
-  const getJobsListAPI = `http://localhost:9002/api`;
-  const updateJobAPI = `http://localhost:9002/api`;
-  const addJobAPI = "http://localhost:9002/api/jobs/addJob";
-  const mainReportAPI = `http://localhost:9002/api/report`;
-  const importerListAPI = "http://localhost:9002/api/getImporterList";
-  const jobsOverviewAPI = "http://localhost:9002/api/getJobsOverview";
-  const importerJobsAPI = "http://localhost:9002/api/getImporterJobs";
-  const assignJobsAPI = "http://localhost:9002/api/assignJobs";
-  const getUsersAPI = "http://localhost:9002/api/getUsers";
-  const getUsersWithJobsAPI = "http://localhost:9002/api/getUsersWithJobs";
-  const updateJobsDateAPI = "http://localhost:9002/api/updateJobsDate";
-  const getLastJobsDateAPI = "http://localhost:9002/api/getLastJobsDate";
-  const importerListToAssignJobs =
-    "http://localhost:9002/api/importerListToAssignJobs";
-  const getYearsAPI = "http://localhost:9002/api/getYears";
-  const removeUserAPI = "http://localhost:9002/api/remove-user";
-  const reportFieldsAPI = "http://localhost:9002/api/getReportFields";
-  const sendOtpAPI = "http://localhost:9002/api/sendOtp";
-  const sendChangePasswordOtpAPI =
-    "http://localhost:9002/api/sendChangePasswordOtp";
-  const getJobAPI = "http://localhost:9002/api/getJob";
-  const feedbackAPI = "http://localhost:9002/api/feedback";
-  const getAssignedImporterAPI =
-    "http://localhost:9002/api/getAssignedImporter";
-  const removeJobsAPI = "http://localhost:9002/api/removeJobs";
-  const changePasswordAPI = "http://localhost:9002/api/changePassword";
-  const downloadReportAPI = "http://localhost:9002/api/downloadReport";
+  // const loginAPI = "http://localhost:9002/api/login";
+  // const registerAPI = "http://localhost:9002/api/register";
+  // const getJobsListAPI = `http://localhost:9002/api`;
+  // const updateJobAPI = `http://localhost:9002/api`;
+  // const addJobAPI = "http://localhost:9002/api/jobs/addJob";
+  // const mainReportAPI = `http://localhost:9002/api/report`;
+  // const importerListAPI = "http://localhost:9002/api/getImporterList";
+  // const jobsOverviewAPI = "http://localhost:9002/api/getJobsOverview";
+  // const importerJobsAPI = "http://localhost:9002/api/getImporterJobs";
+  // const assignJobsAPI = "http://localhost:9002/api/assignJobs";
+  // const getUsersAPI = "http://localhost:9002/api/getUsers";
+  // const getUsersWithJobsAPI = "http://localhost:9002/api/getUsersWithJobs";
+  // const updateJobsDateAPI = "http://localhost:9002/api/updateJobsDate";
+  // const getLastJobsDateAPI = "http://localhost:9002/api/getLastJobsDate";
+  // const importerListToAssignJobs =
+  //   "http://localhost:9002/api/importerListToAssignJobs";
+  // const getYearsAPI = "http://localhost:9002/api/getYears";
+  // const removeUserAPI = "http://localhost:9002/api/remove-user";
+  // const reportFieldsAPI = "http://localhost:9002/api/getReportFields";
+  // const sendOtpAPI = "http://localhost:9002/api/sendOtp";
+  // const sendChangePasswordOtpAPI =
+  //   "http://localhost:9002/api/sendChangePasswordOtp";
+  // const getJobAPI = "http://localhost:9002/api/getJob";
+  // const feedbackAPI = "http://localhost:9002/api/feedback";
+  // const getAssignedImporterAPI =
+  //   "http://localhost:9002/api/getAssignedImporter";
+  // const removeJobsAPI = "http://localhost:9002/api/removeJobs";
+  // const changePasswordAPI = "http://localhost:9002/api/changePassword";
+  // const downloadReportAPI = "http://localhost:9002/api/downloadReport";
 
   /////////////////////////////////////////////////////////////////////////////////////////////////
 
   // server
-  // const loginAPI = "http://15.206.69.192:9002/api/login";
-  // const registerAPI = "http://15.206.69.192:9002/api/register";
-  // const getJobsListAPI = `http://15.206.69.192:9002/api`;
-  // const updateJobAPI = `http://15.206.69.192:9002/api`;
-  // const addJobAPI = "http://15.206.69.192:9002/api/jobs/addJob";
-  // const mainReportAPI = `http://15.206.69.192:9002/api/report`;
-  // const importerListAPI = "http://15.206.69.192:9002/api/getImporterList";
-  // const jobsOverviewAPI = "http://15.206.69.192:9002/api/getJobsOverview";
-  // const importerJobsAPI = "http://15.206.69.192:9002/api/getImporterJobs";
-  // const assignJobsAPI = "http://15.206.69.192:9002/api/assignJobs";
-  // const getUsersAPI = "http://15.206.69.192:9002/api/getUsers";
-  // const getUsersWithJobsAPI = "http://15.206.69.192:9002/api/getUsersWithJobs";
-  // const updateJobsDateAPI = "http://15.206.69.192:9002/api/updateJobsDate";
-  // const getLastJobsDateAPI = "http://15.206.69.192:9002/api/getLastJobsDate";
-  // const importerListToAssignJobs =
-  //   "http://15.206.69.192:9002/api/importerListToAssignJobs";
-  // const getYearsAPI = "http://15.206.69.192:9002/api/getYears";
-  // const removeUserAPI = "http://15.206.69.192:9002/api/remove-user";
-  // const reportFieldsAPI = "http://15.206.69.192:9002/api/getReportFields";
-  // const sendOtpAPI = "http://15.206.69.192:9002/api/sendOtp";
-  // const sendChangePasswordOtpAPI =
-  //   "http://15.206.69.192:9002/api/sendChangePasswordOtp";
-  // const getJobAPI = "http://15.206.69.192:9002/api/getJob";
-  // const feedbackAPI = "http://15.206.69.192:9002/api/feedback";
-  // const getAssignedImporterAPI =
-  //   "http://15.206.69.192:9002/api/getAssignedImporter";
-  // const removeJobsAPI = "http://15.206.69.192:9002/api/removeJobs";
-  // const changePasswordAPI = "http://15.206.69.192:9002/api/changePassword";
-  // const downloadReportAPI = "http://15.206.69.192:9002/api/downloadReport";
+  const loginAPI = "http://15.206.69.192:9002/api/login";
+  const registerAPI = "http://15.206.69.192:9002/api/register";
+  const getJobsListAPI = `http://15.206.69.192:9002/api`;
+  const updateJobAPI = `http://15.206.69.192:9002/api`;
+  const addJobAPI = "http://15.206.69.192:9002/api/jobs/addJob";
+  const mainReportAPI = `http://15.206.69.192:9002/api/report`;
+  const importerListAPI = "http://15.206.69.192:9002/api/getImporterList";
+  const jobsOverviewAPI = "http://15.206.69.192:9002/api/getJobsOverview";
+  const importerJobsAPI = "http://15.206.69.192:9002/api/getImporterJobs";
+  const assignJobsAPI = "http://15.206.69.192:9002/api/assignJobs";
+  const getUsersAPI = "http://15.206.69.192:9002/api/getUsers";
+  const getUsersWithJobsAPI = "http://15.206.69.192:9002/api/getUsersWithJobs";
+  const updateJobsDateAPI = "http://15.206.69.192:9002/api/updateJobsDate";
+  const getLastJobsDateAPI = "http://15.206.69.192:9002/api/getLastJobsDate";
+  const importerListToAssignJobs =
+    "http://15.206.69.192:9002/api/importerListToAssignJobs";
+  const getYearsAPI = "http://15.206.69.192:9002/api/getYears";
+  const removeUserAPI = "http://15.206.69.192:9002/api/remove-user";
+  const reportFieldsAPI = "http://15.206.69.192:9002/api/getReportFields";
+  const sendOtpAPI = "http://15.206.69.192:9002/api/sendOtp";
+  const sendChangePasswordOtpAPI =
+    "http://15.206.69.192:9002/api/sendChangePasswordOtp";
+  const getJobAPI = "http://15.206.69.192:9002/api/getJob";
+  const feedbackAPI = "http://15.206.69.192:9002/api/feedback";
+  const getAssignedImporterAPI =
+    "http://15.206.69.192:9002/api/getAssignedImporter";
+  const removeJobsAPI = "http://15.206.69.192:9002/api/removeJobs";
+  const changePasswordAPI = "http://15.206.69.192:9002/api/changePassword";
+  const downloadReportAPI = "http://15.206.69.192:9002/api/downloadReport";
 
   return {
     loginAPI,
